654668cb76442b15c889f82f87c87f5c24a3a94e
JYC <46228100+sharkbig@users.noreply.github.com>
2020-04-14 21:59:59 +0800

    Add files via upload
    export IW and burst

---
+++ IWs.py
@@ -0,0 +1,80 @@
+import numpy as np
+import zipfile, glob
+from inSide import inSide
+from sys import argv
+
+
+inputfile="project.conf"
+#argv[1]
+fp=open(inputfile)
+for line in fp.readlines():
+	if "PROJECTFOLDER" in line:
+		PROJECT = line.split('=')[1].strip()
+	if "LONMIN" in line:
+		LONMIN = line.split('=')[1].strip()
+	if "LATMIN" in line:
+		LATMIN = line.split('=')[1].strip()
+	if "LONMAX" in line:
+		LONMAX = line.split('=')[1].strip()
+	if "LATMAX" in line:
+		LATMAX = line.split('=')[1].strip()
+
+
+
+
+# polygon='POLYGON (('+LONMIN+' '+LATMIN+','+LONMAX+' '+LATMIN+','+LONMAX+' '+LATMAX+','+LONMIN+' '+LATMAX+','+LONMIN+' '+LATMIN+'))'
+polygon=[[float(LONMIN),float(LATMIN)],[float(LONMIN),float(LATMAX)],[float(LONMAX),float(LATMAX)],[float(LONMAX),float(LATMAX)]]
+
+
+zips=glob.glob("slaves/*/*.zip").sort()
+
+for slave in zips:
+	lsname=zipfile.ZipFile(slave).namelist()
+	vv= []
+	for i in lsname:
+		seg=i.split('/')
+		if "annotation" == seg[1]: 
+			if "vv" in seg[2]:
+				vv.append(i)
+
+	for iw in range(len(vv)):
+		SWs=zipfile.ZipFile(slave).open(vv[iw],'r')
+		count=0
+		line=True
+		tag=False
+		lats=[]
+		lons=[]
+		while line:
+			line=SWs.readline().strip()
+			if b"<geolocationGridPoint>" == line:
+				tag=True
+			elif (b"</geolocationGridPoint>" == line):
+				tag=False
+				continue
+
+			if b"<latitude>" in line and  tag:
+				lat=float(line.split(b'</')[0].split(b">")[1])
+				lats.append(lat)
+			elif b"<longitude>" in line and tag:
+				lon=float(line.split(b'</')[0].split(b">")[1])
+				lons.append(lon)
+				# plt.plot(lon,lat,color[i]+'o', ms=1)
+				count+=1
+		# symbols for ascending but for descending works
+		nr=11
+		nc=21
+		maxbst=-2
+		minbst=12
+		for i in zip(lons,lats):
+			if inSide(polygon,[i[0],i[1]]):
+				bst=lats.index(i[1])//21
+				if bst>=maxbst:
+					maxbst=bst
+				if bst<=minbst:
+					minbst=bst
+
+		if minbst==12: minbst=-1
+		print(vv[iw][88:91]+  "\t" + "%i\t%i"%(minbst,maxbst+1), end="\t")
+		SWs.close()
+
+	print()
