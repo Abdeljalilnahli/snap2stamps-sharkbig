ac0f04b6ae3cf7b68bb7a39cadfd2245d7ff1753
sharkbig <james15156@gmail.com>
2020-04-17 11:56:12 +0800

    add decision for generate graph2run.xml

--- splitting_slaves.py
+++ splitting_slaves.py
@@ -2,15 +2,17 @@
 # Author Jose Manuel Delgado Blasco
 # Date: 21/06/2018
 # Version: 1.0
-
 # Step 1 : preparing slaves in folder structure
 # Step 2 : TOPSAR Splitting (Assembling) and Apply Orbit
 # Step 3 : Coregistration and Interferogram generation
 # Step 4 : StaMPS export
-
 # Added option for CACHE and CPU specification by user
 # Planned support for DEM selection and ORBIT type selection 
 
+
+# case 1: 1 swath in one slice 
+# case 2: 2 swath in one slice -> apply slice assembly 
+# case 3: 1 swath in two slice -> need merge
 
 import os
 from pathlib import Path
@@ -20,32 +22,39 @@ import subprocess
 import shlex
 import time
 import zipfile
-
+from IWs import getBurst
 # inputfile = sys.argv[1]
 inputfile="project.conf"
 bar_message='\n#####################################################################\n'
 
 # Getting configuration variables from inputfile
 try:
-        in_file = open(inputfile, 'r')
-
-        for line in in_file.readlines():
-                if "PROJECTFOLDER" in line:
-                        PROJECT = line.split('=')[1].strip()
-                        print PROJECT
-                if "IWs" in line:
-                        IW = line.split('=')[1].strip()
-                        print IW
-                if "GRAPHSFOLDER" in line:
-                        GRAPH = line.split('=')[1].strip()
-                        print GRAPH
-                if "GPTBIN_PATH" in line:
-                        GPT = line.split('=')[1].strip()
-                        print GPT
-		if "CACHE" in line:
-			CACHE = line.split('=')[1].strip()
-		if "CPU" in line:
-			CPU = line.split('=')[1].strip()
+    in_file = open(inputfile, 'r')
+    for line in in_file.readlines():
+        if "PROJECTFOLDER" in line:
+            PROJECT = line.split('=')[1].strip()
+            print PROJECT
+        if "IWs" in line:
+            IW = line.split('=')[1].strip()
+            print IW
+        if "GRAPHSFOLDER" in line:
+            GRAPH = line.split('=')[1].strip()
+            print GRAPH
+        if "GPTBIN_PATH" in line:
+            GPT = line.split('=')[1].strip()
+            print GPT
+        if "LONMIN" in line:
+            LONMIN = line.split('=')[1].strip()
+        if "LATMIN" in line:
+            LATMIN = line.split('=')[1].strip()
+        if "LONMAX" in line:
+            LONMAX = line.split('=')[1].strip()
+        if "LATMAX" in line:
+            LATMAX = line.split('=')[1].strip()
+        if "CACHE" in line:
+            CACHE = line.split('=')[1].strip()
+        if "CPU" in line:
+            CPU = line.split('=')[1].strip()
 finally:
         in_file.close()
 
@@ -63,6 +72,8 @@ if not os.path.exists(logfolder):
 if not os.path.exists(graphfolder):
                 os.makedirs(graphfolder)
 
+polygon=[[float(LONMIN),float(LATMIN)],[float(LONMIN),float(LATMAX)],[float(LONMAX),float(LATMAX)],[float(LONMAX),float(LATMIN)]]
+
 
 graph2run=PROJECT+'/graphs/splitgraph2run.xml'
 outlog=logfolder+'/split_proc_stdout.log'
@@ -77,94 +88,72 @@ out_file.write(message)
 print bar_message
 out_file.write(bar_message)
 
-if IW=="IW_list":
-    import IWs
-else:
-    with open("logs/IW_list","w") as make_ls:
-        for i  in sorted(glob.glob(slavefolder+"/*/*.zip")):
-            print(i+"\t"+IW+"\t1\t10")
-            make_ls.write(i+"\t"+IW+"\t1\t10\n")
-
-acdatefolder=[]
-IW=[]
-min_bst=[]
-max_bst=[]
-with open("logs/IW_list",'r') as fp:
-    print(fp)
-    for i in fp.readlines():
-        print("hello",i)
-        i=i.split()
-        acdatefolder+=[i[0]]
-        IW+=[i[1]]
-        min_bst+=[i[2]]
-        max_bst+=[i[3]]
-print(IW,min_bst)
-sys.exit()
 
 
 k=0
+
+    
 for acdatefolder in sorted(os.listdir(slavefolder)):
     k=k+1
-    # screen output and logit
     print '['+str(k)+'] Folder: '+acdatefolder
     out_file.write('['+str(k)+'] Folder: '+acdatefolder+'\n')
     print os.path.join(slavefolder, acdatefolder)
     out_file.write(str(os.path.join(slavefolder, acdatefolder))+'\n')
-
-    # zip file to read --> should be replace by the code from 
     files = glob.glob(os.path.join(slavefolder, acdatefolder) + '/*.zip')
+    print files
     out_file.write(str(files)+'\n')
-
     head, tail = os.path.split(os.path.join(str(files)))
     splitslavefolder=splitfolder+'/'+tail[17:25]
     if not os.path.exists(splitslavefolder):
                 os.makedirs(splitslavefolder)
+
+    # need new nameing method
     outputname=tail[17:25]+'_'+IW+'.dim'
-
-
 
     # slave_split_applyorbit
     '''
     Read -> TOPSAR-Split -> Apply-Orbit-File -> Write
             IWs/VV/1-9999
     '''
-    if len(files) == 1 :
-	graphxml=GRAPH+'/slave_split_applyorbit.xml'
-       # Read in the file
+    if len(files) == 1:
+    	graphxml=GRAPH+'/slave_split_applyorbit_readIWs.xml'
+        # Read in the file
         print 'FILE(s) : '+files[0]
-        with open(graphxml, 'r') as file :
-           filedata = file.read()
-        # Replace the target string
-        filedata = filedata.replace('INPUTFILE', files[0])
-	filedata = filedata.replace('IWs',IW)
-	filedata = filedata.replace('OUTPUTFILE',splitslavefolder+'/'+outputname)
-	'''
-	add more parameter to determinate swath and burst
-	'''
-       # # Write the file out again
-        with open(graph2run, 'w') as file:
-           file.write(filedata)
+        
+        gb=getBurst(polygon,files[0])
+        for swi in gb:
+            if gb[swi]==[-1,-1]:
+                continue
+            IWi=swi[88:91].upper()
+            with open(graphxml, 'r') as file :
+                filedata = file.read()
 
-    
+            # Replace the target string
+            filedata = filedata.replace('INPUTFILE', files[0])
+            filedata = filedata.replace('IWs',IWi)
+            filedata = filedata.replace('OUTPUTFILE',splitslavefolder+'/'+outputname)
+            filedata = filedata.replace('BST0',str(gb[swi][0]))
+            filedata = filedata.replace('BST1',str(gb[swi][1]))
+
+            # Write the file out again
+            with open(graph2run, 'w') as file:
+               file.write(filedata)
 
     # graph: slaves_assamble_split_applyorits
-    '''
-    read+read(2) -> SliceAssambly -> TOPSAR-Split -> Apply-Orbit-File -> Write
-    Inputfile                    IWs/VV/burst 1-9999
-    '''                                  
+    # use more than two "slice" for one swath (testing)
     if len(files) > 1 :
-	graphxml=GRAPH+'/slaves_assemble_split_applyorbit.xml'
+    	graphxml=GRAPH+'/slaves_assemble_split_applyorbit.xml'
         with open(graphxml, 'r') as file :
            filedata = file.read()
         # Replace the target string
         filedata = filedata.replace('INPUTFILE1', files[0])
 
-	filedata = filedata.replace('INPUTFILE2', files[1])
-	filedata = filedata.replace('IWs',IW)
-    # filedata=filedata.replace("IWs",IW)
-    filedata = filedata.replace('OUTPUTFILE',splitslavefolder+'/'+outputname)
-    with open(graph2run, 'w') as file:
-        file.write(filedata)
+    	filedata = filedata.replace('INPUTFILE2', files[1])
+    	filedata = filedata.replace('IWs',IW)
+        # filedata=filedata.replace("IWs",IW)
+        filedata = filedata.replace('OUTPUTFILE',splitslavefolder+'/'+outputname)
+        with open(graph2run, 'w') as file:
+            file.write(filedata)
 
 
 
